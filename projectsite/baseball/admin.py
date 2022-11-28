6070516f4e3862c12c3981ec906a8d93f8cf6bb4
JamesAbalus <202080505@psu.palawan.edu.ph>
2022-11-28 13:06:48 +0800

    updated

--- projectsite/baseball/admin.py
+++ projectsite/baseball/admin.py
@@ -1,26 +1,29 @@
 from django.contrib import admin
 from django.contrib.admin import display
-from .models import Position, Person, Club, Play
+from .models import Position, Person, Club, Play, Match
 
+
+# Register your models here.
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ("description",)
     search_fields = ("description",)
 
-
 @admin.register(Person)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("lastname", "firstname", "height", "weight",)
-    search_fields =("lastname", "firstname", "height", "weight",)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("lastname","firstname","height","weight")
+    search_fields = ("lastname",)
 
 @admin.register(Club)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("name", "coach", "dorm_latitude", "dorm_longitude",)
-    search_fields = ("name", "coach", "dorm_latitude", "dorm_longitude",)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("name","coach",)
+    search_fields = ("name",)
 
 @admin.register(Play)
 class PlayAdmin(admin.ModelAdmin):
     list_display = ("player","team","string_no","isActive",)
 
-# Register your models here.
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ("team1","score_t1","team2","score_t2","winner",)
